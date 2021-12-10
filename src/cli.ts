6e0acc382a3fdff35b2ec86c61ac75cece369874
devthejo <jo@surikat.pro>
2021-12-10 12:57:13 +0100

    feat(commander): replace buggy oclif

--- src/cli.ts
+++ src/cli.ts
@@ -1,24 +1,13 @@
-import { Command, flags } from "@oclif/command";
+import { Command } from "commander";
 
 import { generate } from ".";
 
-class EnvSlug extends Command {
-  static description =
-    "Generate slug for K8S namespace or DB name from git branch";
+const program = new Command();
 
-  static flags = {
-    help: flags.help({ char: "h" }),
-    version: flags.version({ char: "v" }),
-  };
+program.argument("<projectBranch>").action((projectBranch: string): void => {
+  console.log(generate(projectBranch));
+});
 
-  static args = [{ name: "project-branch", required: true }];
-
-  // eslint-disable-next-line @typescript-eslint/require-await
-  async run(): Promise<void> {
-    const { args } = this.parse(EnvSlug);
-    const projectBranch: string = args["project-branch"];
-    this.log(generate(projectBranch));
-  }
+export function run(): void {
+  program.parse(process.argv);
 }
-
-export default EnvSlug;
